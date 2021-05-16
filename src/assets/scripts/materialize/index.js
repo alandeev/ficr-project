6f4896fed982ee69430de0d631a7f9054b1b6439
Henrique Black <67487181+hnrqblck@users.noreply.github.com>
2021-05-16 14:39:40 -0300

    navbar-color|carousel positioning

--- src/assets/scripts/materialize/index.js
+++ src/assets/scripts/materialize/index.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
+  
 });
 
 
 const carouselOptions = {
   duration: 100
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
